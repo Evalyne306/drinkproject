ff5d272a94abea284515c0e70ab64728b024437c
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-12-15 17:15:01 +0300

    attaching images

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
 
-//assigning variables to our button and input html elements
-/*const buttonElement = document.querySelector("#search");
-const inputElement = document.querySelector("#input");
 
-buttonElement.onclick =function(e){
-    e.preventDefault()
+//  let outcome =document.getElementById("outcome");
+//  let searchButton = document.getElementById("searchbtn");
+//  let url ="https://api.npoint.io/87c45ca63ee422e2ccee/drinks"
+//  let userText = document.getElementById("user-text").value;
 
-    console.log("Hello beautiful")
-    const value = inputElement.value;
-    console.log("Value:", value)
-
-fetch("https://api.npoint.io/b8d62bffb58f3c1e3754/drinks")
-.then((res)=>res.json())
-.then((data)=>{
-
-
-console.log("Data:", data);
-    
-})
- }
- */
-let selectedDrink = null;
- let outcome =document.getElementById("outcome");
- let searchButton = document.getElementById("searchbtn");
- let url ="https://api.npoint.io/b8d62bffb58f3c1e3754/drinks"
- let userText = document.getElementById("user-text").value;
-//  if (userText.length == 0){
-// outcome.innerHTML=`<h3 class="msg">The field cannot be empty</h3>`
-//  }
-//  else{
-    fetch(url)
-    .then((res)=>res.json())
-    .then((data)=>{
-        console.log(data)
+//     fetch(url)
+//     .then((res)=>res.json())
+//     .then((data)=>{
+//         console.log(data)
        
-    });
+//  
+console.info("cocktails loaded");  
+// document.addEventListener("prechange",function(event){
+//     document.querySelector("")
+//     .innerHTML = event.tabItem.getAttribute("label");
+// });
+function getRandomCocktails(){
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(
+    function(response) {
+        if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+        return;
+        }
 
+        // Examine the text in the response
+        response.json().then(function(data) {
+        //console.log(data);
+        displayRandomCocktail(data);
+        });
+    }
+    )
+    .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+    });  
+}
+getRandomCocktails()
 
+function displayRandomCocktail(cocktail){
+ console.log(cocktail.drinks[0])
 
+ let drinkSection = document.querySelector("#drink-section")
+ let drinkName = document.createElement("h2");
+ drinkName.innerHTML = cocktail.drinks[0].strDrink;
+ drinkSection.appendChild(drinkName);
+
+ let img = document.createElement("img")
+ img.src = cocktail.drinks[0].strDrinkThumb;
+
+ drinkSection.appendChild(img);
+ for(let i=1; i<16; i++){
+    console.log();
+    if(cocktail.drinks[0][`strIngredient${i}`] == null || cocktail.drinks[0][`strIngredient${i}`]==""){
+        break;
+    }
     
+    let ingredients = document.querySelector("#ingredients")
+    let ingredient = document.createElement("li")
+    ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] +":" + cocktail.drinks[0][`strIngredient${i}`]
 
-// function showDrink(){
-//     console.log("fhgfhfj", selectedDrink)
-//     const strDrink
-//     const strCategory
-//     const strIngredient1
+    ingredients.appendChild(ingredient)
+    
+}  
+let instructions = document.createElement("i");
+instructions.innerHTML = cocktail.drinks[0].strInstructions;
+
+ingredients.appendChild(instructions);
+
+}
 
 
