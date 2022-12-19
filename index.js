265df6c9bf8cc6cc33e871379a873fdf683e00ea
Evalyne306 <evalyne.wambui@student.moringaschool.com>
2022-12-19 15:49:00 +0300

    added sign up form

--- index.js
+++ index.js
@@ -1,22 +1,55 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    event.preventDefault();
 
- let getInfo = ()=>{
-const form = document.getElementById("form")
+
+const container = document.getElementById("container");
+const signup = document.getElementById("signup")
+const login = document.getElementById("login")
 const app = document.getElementById("app")
+const btnsignup = document.getElementById("btnsignup")
 const btnlogin = document.getElementById("btnlogin")
-btnlogin.addEventListener("click",()=>{
-    app.removeAttribute('hidden')
-    app.style.display = "flex"
-    form.style.display = "none"
+
+btnsignup.addEventListener('click',(event)=>{
+    event.preventDefault()
+    login.removeAttribute('hidden')
+    login.style.display = "block"
+    app.style.display = "none"
+    signup.style.display = "none"
+    
 })
+btnlogin.addEventListener('click',(event)=>{
+    event.preventDefault()
+    login.style.display ="none"
+    signup.style.display = "none"
+    app.style.display = "block"
+    app.removeAttribute('hidden')
+
+})
+
+const loadimages = document.getElementById("loadimages")
+loadimages.addEventListener('click',(event)=>{
+event.preventDefault()
+getInfo()
+const ingredients = document.getElementById("ingredients")
+ingredients.innerHTML = ""
+const drinkSection = document.getElementById("drink-section")
+drinkSection.innerHTML = ""
+})
+
+const getInfo = ()=>{
+
+
+
+
+
+
+
+
 function getRandomCocktails(){
 fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     .then(
     function(response) {
-        if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-        return;
-        }
+
 
         // Examine the text in the response
         response.json().then(function(data) {
@@ -30,19 +63,17 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
     });  
 }
 
-getRandomCocktails()
-
-
+ getRandomCocktails()
 
 function displayRandomCocktail(cocktail){
  console.log(cocktail.drinks[0])
 
- let drinkSection = document.querySelector("#drink-section")
- let drinkName = document.createElement("h2");
+ const drinkSection = document.querySelector("#drink-section")
+ const drinkName = document.createElement("h2");
  drinkName.innerHTML = cocktail.drinks[0].strDrink;
  drinkSection.appendChild(drinkName);
 
- let img = document.createElement("img")
+ const img = document.createElement("img")
  img.src = cocktail.drinks[0].strDrinkThumb;
 
  drinkSection.appendChild(img);
@@ -52,19 +83,24 @@ function displayRandomCocktail(cocktail){
         break;
     }
     
-    let ingredients = document.querySelector("#ingredients")
-    let ingredient = document.createElement("li")
+    const ingredients = document.querySelector("#ingredients")
+    const ingredient = document.createElement("li")
     ingredient.innerHTML = cocktail.drinks[0][`strMeasure${i}`] +":" + cocktail.drinks[0][`strIngredient${i}`]
 
     ingredients.appendChild(ingredient)
     
 }  
-let instructions = document.createElement("h5");
+const instructions = document.createElement("h5");
 instructions.innerHTML = cocktail.drinks[0].strInstructions;
 
 ingredients.appendChild(instructions);
 
 }
 
+
 };
+
 window.addEventListener("load", getInfo);
+
+
+});
